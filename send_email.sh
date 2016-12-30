abc4f67b394865e9a9fb400613294b7a6ec8ee08
smrckln <svr5263@psu.edu>
2016-12-29 20:59:03 -0500

    fixing malformed attachment file name

--- send_email.sh
+++ send_email.sh
@@ -1,3 +1,3 @@
 bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty
-echo "Groupme Log File" | mail -s "`date`" -a "groupme.log.pretty" $LOG_EMAIL
+echo "Groupme Log File" | mail -s "`date`" -a groupme.log.pretty $LOG_EMAIL
 rm groupme.log.pretty
