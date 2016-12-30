c122b99dd311c91c80f2ee0e08320b4699f59b7b
smrckln <svr5263@psu.edu>
2016-12-29 21:33:39 -0500

    apparently environment variables don't work in scripts

--- send_email.sh
+++ send_email.sh
@@ -1,3 +1,6 @@
+GROUPME_DIR=
+LOG_EMAIL=
+
 echo "bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty"
 bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty
 echo "Groupme Log File" | mail -s "`date`" -A groupme.log.pretty $LOG_EMAIL
