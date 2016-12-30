27c6ed4a8b2a82a0f52a0bfd2c1bd3eb62dedac4
smrckln <svr5263@psu.edu>
2016-12-29 20:56:50 -0500

    crontab should work correctly

--- send_email.sh
+++ send_email.sh
@@ -1,3 +1,3 @@
-bunyan logs/groupme.log.0 > groupme.log.pretty
+bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty
 echo "Groupme Log File" | mail -s "`date`" -a "groupme.log.pretty" $LOG_EMAIL
 rm groupme.log.pretty
