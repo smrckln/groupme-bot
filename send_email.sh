fb2660ee92618ed5181c07cf298d1422c9521bd4
smrckln <svr5263@psu.edu>
2016-12-29 21:16:21 -0500

    adding debug prints to shell script

--- send_email.sh
+++ send_email.sh
@@ -1,3 +1,4 @@
+echo "bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty"
 bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty
 echo "Groupme Log File" | mail -s "`date`" -A groupme.log.pretty $LOG_EMAIL
 rm groupme.log.pretty
