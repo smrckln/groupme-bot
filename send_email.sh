aa385bd8c388d299ed2b260212946f4476ed5b94
smrckln <svr5263@psu.edu>
2017-01-01 14:25:36 -0500

    Script should now work correctly from cron

--- send_email.sh
+++ send_email.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+source /root/.bashrc
+
 echo "bunyan $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty"
 `which bunyan` $GROUPME_DIR/logs/groupme.log.0 > groupme.log.pretty
 echo "Groupme Log File" | `which mail` -s "`date`" -A groupme.log.pretty $LOG_EMAIL
